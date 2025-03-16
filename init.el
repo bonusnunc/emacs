42c4f9799a80afb5ec3c1cc4bfafa12e99c6962f
Martín Buenahora <martinbuenahora@gmal.com>
2025-03-16 09:36:52 -0500

    Arreglando nyan-mode y otros

--- init.el
+++ init.el
@@ -7,7 +7,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(adaptive-wrap all-the-icons auctex auto-complete auto-dark dashboard
+		   doom-modeline ef-themes gruvbox-theme ligature
+		   modus-themes nyan-mode org-appear org-bullets
+		   org-modern ox-reveal poke-line standard-themes
+		   ultra-scroll visual-fill-column yasnippet))
  '(package-vc-selected-packages
    '((ultra-scroll :vc-backend Git :url
 		   "https://github.com/jdtsmith/ultra-scroll"))))
